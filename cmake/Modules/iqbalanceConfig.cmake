4386cb8af0446744e4ef0e9aa26d95263050ddde
Vasil Velichkov <vvvelichkov@gmail.com>
2019-09-01 22:09:46 +0300

    Use gnuradio-iqbalanceTargets.cmake in iqbalanceConfig.cmake
    Model it after volk/cmake/Modules/VolkConfig.cmake.in

--- cmake/Modules/iqbalanceConfig.cmake
+++ cmake/Modules/iqbalanceConfig.cmake
@@ -1,29 +1,32 @@
-INCLUDE(FindPkgConfig)
-PKG_CHECK_MODULES(PC_IQBALANCE iqbalance)
+if(NOT TARGET gnuradio::gnuradio-iqbalance)
+    include("${CMAKE_CURRENT_LIST_DIR}/gnuradio-iqbalanceTargets.cmake")
+endif()
 
-FIND_PATH(
-    IQBALANCE_INCLUDE_DIRS
-    NAMES gnuradio/iqbalance/api.h
-    HINTS $ENV{IQBALANCE_DIR}/include
-        ${PC_IQBALANCE_INCLUDEDIR}
-    PATHS ${CMAKE_INSTALL_PREFIX}/include
-          /usr/local/include
-          /usr/include
-)
+# set IQBALANCE_FOUND to be set globally, for whether a compatible gnuradio-iqbalance was
+# found -- could be a correct enough version or any version depending
+# on how find_package was called.
+if(NOT TARGET gnuradio::gnuradio-iqbalance)
+    set(IQBALANCE_FOUND FALSE)
+else()
+    set(IQBALANCE_FOUND TRUE)
+endif()
 
-FIND_LIBRARY(
-    IQBALANCE_LIBRARIES
-    NAMES gnuradio-iqbalance
-    HINTS $ENV{IQBALANCE_DIR}/lib
-        ${PC_IQBALANCE_LIBDIR}
-    PATHS ${CMAKE_INSTALL_PREFIX}/lib
-          ${CMAKE_INSTALL_PREFIX}/lib64
-          /usr/local/lib
-          /usr/local/lib64
-          /usr/lib
-          /usr/lib64
-          )
+# cache whether a compatible gnuradio-iqbalance was found for
+# use anywhere in the calling project
+set(IQBALANCE_FOUND ${IQBALANCE_FOUND} CACHE BOOL "Whether a compatible gnuradio-iqbalance was found" FORCE)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(IQBALANCE DEFAULT_MSG IQBALANCE_LIBRARIES IQBALANCE_INCLUDE_DIRS)
-MARK_AS_ADVANCED(IQBALANCE_LIBRARIES IQBALANCE_INCLUDE_DIRS)
+if(IQBALANCE_FOUND)
+    # use the new target library, regardless of whether new or old style
+    # we still need to set a variable with the library name so that there
+    # is a variable to reference in the using-project's cmake scripts!
+    set(IQBALANCE_LIBRARIES gnuradio::gnuradio-iqbalance CACHE STRING "gnuradio-iqbalance Library" FORCE)
+
+    # INTERFACE_INCLUDE_DIRECTORIES should always be set
+    get_target_property(IQBALANCE_INCLUDE_DIRS gnuradio::gnuradio-iqbalance INTERFACE_INCLUDE_DIRECTORIES)
+    set(IQBALANCE_INCLUDE_DIRS ${IQBALANCE_INCLUDE_DIRS} CACHE STRING "gnuradio-iqbalance Include Directories" FORCE)
+
+    # for backward compatibility with old-CMake non-target project finding
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(IQBALANCE DEFAULT_MSG IQBALANCE_LIBRARIES IQBALANCE_INCLUDE_DIRS)
+    mark_as_advanced(IQBALANCE_LIBRARIES IQBALANCE_INCLUDE_DIRS)
+endif(IQBALANCE_FOUND)
